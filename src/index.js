1eac2329f4534322e17e5574dfa511e1955e3176
ihumentsev <106383787+ihumentsev@users.noreply.github.com>
2024-01-26 18:49:56 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import './assets/fonts/fonts.css';
 import { Provider } from 'react-redux';
@@ -10,12 +10,12 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/crm_front">
+    <Router basename="/crm_front">
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
         </PersistGate>
       </Provider>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
