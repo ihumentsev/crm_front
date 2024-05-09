ccf33eb9b4bd79257264c9b16c5d9ab40925467b
ihumentsev <106383787+ihumentsev@users.noreply.github.com>
2024-05-09 15:23:13 +0200

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,25 @@
-// import { BluButton } from "components/Btn/BlueBtn.styled";
-import { BtnWraper } from "./Header.styled";
-import BluButton from '../Btn/BlueBtn'
-export default function Header({ textHead, textBtn, handler, inputValue, setValue }) {
+import { BtnWraper } from './Header.styled';
+import BluButton from '../Btn/BlueBtn';
+export default function Header({
+  textHead,
+  textBtn,
+  handler,
+  inputValue,
+  setValue,
+}) {
   return (
     <BtnWraper>
       <div className="title">{textHead}</div>
       <div>
-          <input  placeholder="Search" type='text' value={inputValue} onChange={(e) =>{setValue(e.target.value)}}/>
-        </div>
+        <input
+          placeholder="Search"
+          type="text"
+          value={inputValue}
+          onChange={e => {
+            setValue(e.target.value);
+          }}
+        />
+      </div>
       <BluButton text={textBtn} handler={handler}></BluButton>
     </BtnWraper>
   );
